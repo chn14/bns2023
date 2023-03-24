94c9c506dd2bf9096775f6f9a5c71b4c9d6f687e
Chn <123629446+chn14@users.noreply.github.com>
2023-03-24 16:39:31 +0100

    Update bns2023.py

--- bns2023.py
+++ bns2023.py
@@ -116,7 +116,129 @@ def a_doublon(tab):
             return True
     return False
 
+def voisinage(n, ligne, colonne):
+    """ Renvoie la liste des coordonnées des voisins de la case
+    (ligne, colonne) en gérant les cases sur les bords. """
+    voisins = []
+    for l in range(max(0,ligne-1), min(n, ligne+2)):
+        for c in range(max(0, colonne-1), min(n, colonne+2)):
+            if (l, c) != (ligne, colonne):
+                voisins.append((l,c))
+    return voisins
 
+
+def incremente_voisins(grille, ligne, colonne):
+    """ Incrémente de 1 toutes les cases voisines d'une bombe."""
+    voisins = voisinage(len(grille),ligne,colonne)
+    for l, c in voisins:
+        if grille[l][c] != -1: # si ce n'est pas une bombe
+            grille[l][c] += 1  # on ajoute 1 à sa valeur
+
+
+
+def genere_grille(bombes):
+    """ Renvoie une grille de démineur de taille nxn où n est
+    le nombre de bombes, en plaçant les bombes à l'aide de
+    la liste bombes de coordonnées (tuples) passée en
+    paramètre. """
+    n = len(bombes)
+    # Initialisation d'une grille nxn remplie de 0
+    grille = [[0 for colonne in range(n)] for ligne in range(n)]
+    # Place les bombes et calcule les valeurs des autres cases
+    for ligne, colonne in bombes:
+        grille[ligne][colonne] = -1 # place la bombe
+        incremente_voisins(grille,ligne,colonne) # incrémente ses voisins
+
+    return grille
+
+# Sujet 5
+
+import random
+
+def lancer(n):
+    tab = []
+    for i in range(n):
+        tab.append(random.randint(1,6))
+    return tab
+
+def paire_6(tab):
+    nb = 0
+    for i in tab:
+        if i == 6:
+            nb += 1
+    if nb >= 2:
+        return True
+    return False
+
+def nbLig(image):
+    '''renvoie le nombre de lignes de l'image'''
+    return len(image)
+
+def nbCol(image):
+    '''renvoie la largeur de l'image'''
+    return len(image[0])
+
+def negatif(image):
+    '''renvoie le negatif de l'image sous la forme
+       d'une liste de listes'''
+
+    # on cree une image de 0 aux memes dimensions que le parametre image
+    L = [[0 for k in range(nbCol(image))] for i in range(nbLig(image))]
+
+    for i in range(nbLig(image)):
+        for j in range(nbCol(image)):
+            L[i][j] = 255-image[i][j]
+    return L
+
+def binaire(image, seuil):
+    '''renvoie une image binarisee de l'image sous la forme
+       d'une liste de listes contenant des 0 si la valeur
+       du pixel est strictement inferieure au seuil
+       et 1 sinon'''
+
+    # on cree une image de 0 aux memes dimensions que le parametre image
+    L = [[0 for k in range(nbCol(image))] for i in range(nbLig(image))]
+
+    for i in range(nbLig(image)):
+        for j in range(nbCol(image)):
+            if image[i][j] < seuil :
+                L[i][j] = 0
+            else:
+                L[i][j] = 1
+    return L
+
+# Sujet 6
+
+def recherche(tab,n):
+    indice = None
+    for i in range(len(tab)):
+        if tab[i]==n:
+            indice = i
+    if indice == None:
+        return len(tab)
+    return indice
+
+from math import sqrt     # import de la fonction racine carrée
+
+def distance(point1, point2):
+    """ Calcule et renvoie la distance entre deux points. """
+    return sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2)
+
+def plus_courte_distance(tab, depart):
+    """ Renvoie le point du tableau tab se trouvant à la plus courte distance du point depart."""
+    point = tab[0]
+    min_dist = distance(point,depart)
+    for i in range (1, len(tab)):
+        if distance(tab[i], depart)<min_dist:
+            point = tab[i]
+            min_dist = distance(tab[i],depart)
+    return point
+
+# Sujet 7
+
+def fusion(tab1, tab2):
+
+            
 
 
 
