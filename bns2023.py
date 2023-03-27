5365e0454ad9ca18b869c2dcaecbfddc659715b6
Chn <123629446+chn14@users.noreply.github.com>
2023-03-27 20:28:28 +0200

    Update bns2023.py

--- bns2023.py
+++ bns2023.py
@@ -236,7 +236,38 @@ def plus_courte_distance(tab, depart):
 
 # Sujet 7
 
-def fusion(tab1, tab2):
+def fusion(tab1,tab2):
+    tab = []
+    i1 = 0
+    i2 = 0
+    while i1 < len(tab1) and i2 < len(tab2):
+        if tab1[i1] < tab2[i2]:
+            tab.append(tab1[i1])
+            i1 += 1
+        else:
+            tab.append(tab2[i2])
+            i2 += 1
+    tab += tab1[i1:] + tab2[i2:]
+    return tab
+
+romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+
+def traduire_romain(nombre):
+    """ Renvoie l’écriture décimale du nombre donné en chiffres
+    romains """
+    if len(nombre) == 1:
+        return romains[nombre]
+    elif romains[nombre[0]] >= romains[nombre[1]]:
+        return romains[nombre[0]] + traduire_romain(nombre[1:])
+    else:
+        return -1*romains[nombre[0]] + traduire_romain(nombre[1:])
+
+            
+#Sujet 8
+        
+        
+            
+        
 
             
 
